c2edac94355735495507989b27167a00f7540666
Joseph Volpe <jlv38@case.edu>
2018-02-23 18:26:26 -0500

    Created heatmap.js template

---
+++ pathfinder_web/client/src/Heatmap.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+class Heatmap extends React.Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    <div>
+      //Add image of heatmap and possible interaction
+    </div>
+  }
+}
+
+export default Heatmap;
