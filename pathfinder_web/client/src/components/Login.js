66dbb1c252fe2f3f9fd57f460e29329d4cc7efee
Joseph Volpe <josephvolpe@case.edu>
2018-02-28 00:17:02 -0500

    more work on login box

--- pathfinder_web/client/src/components/Login.js
+++ pathfinder_web/client/src/components/Login.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone'
 
 class Login extends Component {
-
     render() {
         return (
             <div className="login-box centered">
                 <h2>Pathfinder</h2>
                 <LoginForm />
-
-                <div className="job-id-box">
-
-                </div>
-                <div className="upload-images-box">
-
-                </div>
-
+                <FileDrop />
             </div>
         )
     }
@@ -42,14 +35,42 @@ class LoginForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Job ID:
-                    <input type="text" value={this.state.jobID} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Enter" />
-            </form>
+            <div className="job-id-box">
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Job ID:
+                        <input type="text" value={this.state.jobID} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Enter" />
+                </form>
+            </div>
         )
+    }
+}
+
+class FileDrop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: [],
+        };
+    }
+
+    onDrop(acceptedFiles, rejectedFiles) {
+        this.setState({
+            files: acceptedFiles
+        });
+        console.log(acceptedFiles);
+    }
+
+    render() {
+        return (
+            <div className="upload-images-box">
+                <Dropzone onDrop={(files) => this.onDrop(files)}>
+                    <div>Select .zip with images for analysis</div>
+                </Dropzone>
+            </div>
+        );
     }
 }
 
