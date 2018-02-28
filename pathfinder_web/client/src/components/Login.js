c01f40646ceb3b6d70978c68a0341d173150eb0f
Joseph Volpe <josephvolpe@case.edu>
2018-02-27 23:27:20 -0500

    adding button for submission and textbox for id

--- pathfinder_web/client/src/components/Login.js
+++ pathfinder_web/client/src/components/Login.js
@@ -6,16 +6,49 @@ class Login extends Component {
         return (
             <div className="login-box centered">
                 <h2>Pathfinder</h2>
-                <p>JobID</p>
+                <LoginForm />
+
                 <div className="job-id-box">
 
                 </div>
-                <EnterJobButton />
                 <div className="upload-images-box">
 
                 </div>
 
             </div>
+        )
+    }
+}
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            jobID: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState(
+            {jobID: event.target.value}
+        );
+    }
+
+    handleSubmit(event) {
+
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Job ID:
+                    <input type="text" value={this.state.jobID} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Enter" />
+            </form>
         )
     }
 }
