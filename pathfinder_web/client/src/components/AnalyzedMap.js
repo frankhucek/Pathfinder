ac9ac1b4a358804d88a1e89f97ac6087b46def1e
Joseph Volpe <josephvolpe@case.edu>
2018-02-27 21:44:08 -0500

    concentrated image display to one component as essentially working similairly

---
+++ pathfinder_web/client/src/components/AnalyzedMap.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class AnalyzedMap extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+      <div>
+        //Add image of heatmap or coordinate mappping and possible interaction
+      </div>
+  }
+}
