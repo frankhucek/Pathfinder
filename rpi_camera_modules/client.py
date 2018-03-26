c2265d13a4644350df757105d2172e8285e44cbb
Frank Hucek <frankhucek@case.edu>
2018-03-26 05:28:12 -0400

    now can specify which directory to save file

--- rpi_camera_modules/client.py
+++ rpi_camera_modules/client.py
@@ -12,29 +12,65 @@ remote_host = "localhost" # "frankhucek.com"
 remote_host_port = 3001 # port forwarded and ready to go
 passphrase_file = "pathfinder_gpg_passphrase.txt"
 
+"""
+Sends the specified 'filename' to the remote host
+host=remote_host, port=remote_host_port, pass_file=passphrase_file
+"""
 def send_photo(filename):
     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((remote_host, remote_host_port))
     
-    # send signed data to server and wait for response
-    detached_sig_data = signed_connection_data(filename)
-    s.send(detached_sig_data)
+    send_sig(filename, passphrase_file, s)
+    
     ack = s.recv(1024)
     if ack == b"ACK":
-        file_to_send = open(filename, "rb")
-        data_to_send= file_to_send.read(2048)
-        while data_to_send:
-            s.send(data_to_send)
-            data_to_send = file_to_send.read(2048)
-        #file_to_send.close()
+        send_photo_data(filename, s)
+
+    s.shutdown(socket.SHUT_RDWR)
     s.close()
     return
 
-def signed_connection_data(filename):
+def send_sig(filename, pass_file, conn_socket):
+    # send signed data to server and wait for response
+    detached_sig_data = signed_data(filename, passphrase_file)
+    print("sending detached sig")
+    conn_socket.send(detached_sig_data)
+    return
+
+def send_photo_data(filename, conn_socket):
+    print("sending filename")
+    conn_socket.send(filename.encode())
+    ack = conn_socket.recv(1024)
+    if ack == b"SEND_FILE":
+        send_photo_file(filename, conn_socket)
+    return
+
+"""
+Sends the data contained in 'filename' over 'conn_socket'
+- sent in 2048 byte chunks
+"""
+def send_photo_file(filename, conn_socket):
+    print("sending file")
+    file_to_send = open(filename, "rb")
+    data_to_send= file_to_send.read(2048)
+    while data_to_send:
+        conn_socket.send(data_to_send)
+        data_to_send = file_to_send.read(2048)
+
+    file_to_send.flush()
+    file_to_send.close()
+    return
+
+
+"""
+Signs the data contained in 'filename'
+Returns the detached signature in binary bytestring format
+"""
+def signed_data(filename, pass_file):
     gpg = gnupg.GPG(gnupghome='~/.gnupg')
-    with open(passphrase_file) as f:
+    with open(pass_file) as f:
         with open(filename, "rb") as file_to_sign:
             password = f.read().splitlines()[0]
-            detached_sig = gpg.sign_file(file_to_sign, detach=True, binary=True, passphrase=password)
+            detached_sig = gpg.sign_file(file_to_sign, detach=True, binary=True,passphrase=password)
             return detached_sig.data
             # return b'gonna get denied'
--- rpi_camera_modules/server.py
+++ rpi_camera_modules/server.py
@@ -6,52 +6,97 @@ import io
 import socket
 import struct
 import gnupg # python-gnupg package NOT gnupg package
+import os
+import re
 
 local_host = "localhost"
 local_host_port = 3001 # port forwarded and ready to go
-username_to_verify = "pathfinder_camera"
+verified_usernames = ["pathfinder_camera <pathfinder_camera@frankhucek.com>", "pathfinder_cam_0 <pathfinder_cam_0@frankhucek.com>"]
+#verified_usernames = "pathfinder_cam_0"
+client_sig_file = "client.sig"
+base_job_dir = "/home/frank/Code/srproj/2018_spring_395_pathfinder/jobs/"
+TIMEOUT = 10
 
-def recv_photo(filename, sigfile, gpg, client_socket):
-    #f = open(filename, "wb")
-    data_recv = client_socket.recv(2048)
+def recv_photo(filename, sigfile, client_socket):
+    # create gpg instance
+    gpg = gnupg.GPG(gnupghome='~/.gnupg')
+    
+    data_recv = client_socket.recv(16)
     data = b''
+    print("receiving photo data")
     while data_recv:
         data += data_recv
         data_recv = client_socket.recv(2048)
-
+        
     verified = gpg.verify_data(sigfile, data)
     print("username of sign: " + str(verified.username))
-    if verified.valid and username_to_verify in verified.username:
-        f = open(filename, "wb")
+    if verified.valid and verified.username in ":".join(verified_usernames):
+        job_num = re.search(r'\d+', verified.username).group()
+        job_dir = base_job_dir + job_num + "/"
+        if not job_num:
+            job_num = '0'
+        if not os.path.exists(job_dir):
+            os.makedirs(job_dir)
+        print("received file: " + job_dir + filename)    
+        f = open(job_dir + filename, "wb")
         f.write(data)
         f.close()
+        return
     
 ### temp sig file gets written no matter what is sent in
 def listen_for_photos():
     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
     s.bind((local_host, local_host_port))
-    s.listen(1) # only queue 1 connection right now
-
-    # create gpg instance
-    gpg = gnupg.GPG(gnupghome='~/.gnupg')
+    s.listen(5) # only queue 5 connections right now
     
     while True:
         (client_socket, addr) = s.accept()
-        
-        detached_sig_data = client_socket.recv(2048)
-        print("client connected with sig: " + str(detached_sig_data))
-        sig_file = open("client.sig", "wb")
-        sig_file.write(detached_sig_data)
-        sig_file.close()
+        client_socket.settimeout(TIMEOUT) # set timeout to prevent ddos
+        # client_socket.setblocking(0)
+        try:
+            # get sig data and write to sig file
+            handle_sig_data(client_sig_file, client_socket)
 
-        client_socket.send(b"ACK")
-        print("sent ACK to client")
+            # ACK for signature receieved
+            client_socket.send(b"ACK")
 
-        recv_photo("tempphoto.jpg", "client.sig", gpg, client_socket)
+            # get filename
+            filename = client_socket.recv(1024)
+            filename = filename.decode("utf-8")
+            print("received filename")
+            
+            # ACK for filename received
+            client_socket.send(b"SEND_FILE")
 
-        client_socket.close()
+            # get photo data and write to file if verified
+            recv_photo(filename, client_sig_file, client_socket)
+            
+            if os.path.exists(client_sig_file):
+                os.remove(client_sig_file)
+                
+            client_socket.close()
+        except socket.timeout:
+            print("connection timed out")
+            client_socket.close()
+            continue;
+        except socket.error as e:
+            print("socket error")
+            client_socket.close()
+            continue;
+        except:
+            print("weird error")
+            continue;
         
     s.close()
+    return
+
+def handle_sig_data(sigfile, client_socket):
+    detached_sig_data = client_socket.recv(2048)
+    print("received signature data")
+    sig_file = open(sigfile, "wb")
+    sig_file.write(detached_sig_data)
+    sig_file.close()
     return
 
 listen_for_photos()
