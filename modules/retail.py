ad85381aa33ec3d4b471b1fd3027f31cd6348d32
Joseph Volpe <jlv38@case.edu>
2018-04-20 21:57:46 -0400

    cli for retail importing heatmap

--- modules/retail.py
+++ modules/retail.py
@@ -1,7 +1,37 @@
 
+# Import
+import argparse
+from heatmap import Heatmap
+
+# Class
 class RetailSuggestion():
 
+    def __init__(self, heatmap_data):
+        super(RetailSuggestion, self).__init__()
+        self.heatmap_data = heatmap_data
 
+    def suggestions(self):
+        pass
+
+# Helpers
+def create_retail(heatmap):
+    hm = Heatmap.load(heatmap_filepath)
+    retail = RetailSuggestion(hm)
+    retail.suggestions()
+
+# Main
+def main():
+    args = get_args()
+    if args.op == "create_retail":
+        create_retail(args.image_filepath)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("op",
+                        choices=["create_retail"])
+    parser.add_argument("image_filepath",
+                        help="Image files")
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
