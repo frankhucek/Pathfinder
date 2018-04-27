3dee533e1af70b9070015efc8617cb7108be7655
Frank Hucek <frankhucek@case.edu>
2018-04-27 09:21:30 -0400

    Finished all client side scripts. run with main in capture.py. make sure server has script running already.

--- camera/capture.py
+++ camera/capture.py
@@ -5,15 +5,43 @@
 
 import time
 import picamera
+import client
+
+CAPTURE_JOB_DIR = "job_captures/0"
+DEFAULT_PHOTO_COUNT = 10
 
 def capture(resolutionX, resolutionY, photo_number, job_directory):
+    fname = ""
     with picamera.PiCamera() as camera:
         camera.resolution = (resolutionX,resolutionY)
         camera.start_preview()
         # Camera init time slightly > 1 second
         time.sleep(2)
-        filename = "job" + job_directory[-1] + "_" + str(photo_number) + ".jpg"
+        filename = str(photo_number) + ".jpg"
+        fname = filename
         camera.capture(job_directory + "/" + filename)
-    return
+    return fname
 # usage:
 # capture(1024, 768, 0, "jobs/0")
+# dir: job_captures/0
+
+def capture_and_send_series(photo_count, job_dir):
+
+    i = 0
+    while i < photo_count:
+        print("CAPTURING IMG #" + str(i))
+        filename = capture(1024, 768, i, job_dir)
+        print("SENDING " + filename)
+        if filename != "":
+            client.send_photo(job_dir + "/" + filename)
+        i+=1
+            
+    return
+
+def main():
+    print("STARTING CAPTURE AND SENDING OF PHOTOS TO REMOTE SERVER")
+    capture_and_send_series(DEFAULT_PHOTO_COUNT, CAPTURE_JOB_DIR)
+    print("FINISHED SENDING PHOTOS TO SERVER. CLOSING CLIENT APP")
+
+if __name__ == "__main__":
+    main()
