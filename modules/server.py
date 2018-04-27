90391047b594fc840c0a90412bcafb1888573405
Frank Hucek <frank@xeneon.frankhucek.com>
2018-04-26 22:02:42 -0400

    fixed stuff to run on xeneon

--- modules/server.py
+++ modules/server.py
@@ -1,7 +1,6 @@
 """
 This script runs on the remote server to receive an image from a raspberry pi running the client script.
 """
-
 import io
 import socket
 import struct
@@ -10,7 +9,8 @@ import os
 import re
 
 import jobmanager
-local_host = "localhost"
+
+local_host = ""
 local_host_port = 3001 # port forwarded and ready to go
 verified_usernames = ["pathfinder_camera <pathfinder_camera@frankhucek.com>", "pathfinder_cam_0 <pathfinder_cam_0@frankhucek.com>"]
 #verified_usernames = "pathfinder_cam_0"
@@ -21,36 +21,39 @@ TIMEOUT = 10
 def recv_photo(filename, sigfile, client_socket):
     # create gpg instance
     gpg = gnupg.GPG()
-    
+
     data_recv = client_socket.recv(16)
     data = b''
     print("receiving photo data")
     while data_recv:
         data += data_recv
         data_recv = client_socket.recv(2048)
-        
+    print("received all photo data")
+
     verified = gpg.verify_data(sigfile, data)
+
     print("username of sign: " + str(verified.username))
     if verified.valid and verified.username in ":".join(verified_usernames):
+        print("VERIFIED SIGNATURE")
         job_num = re.search(r'\d+', verified.username).group()
         job_dir = base_job_dir + job_num + "/images/"
         if not job_num:
             job_num = '0'
         if not os.path.exists(job_dir):
             os.makedirs(job_dir)
-        print("received file: " + job_dir + filename)    
+        print("received file: " + job_dir + filename)
         f = open(job_dir + filename, "wb")
         f.write(data)
         f.close()
         return job_dir
-    
+
 ### temp sig file gets written no matter what is sent in
 def listen_for_photos():
     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
     s.bind((local_host, local_host_port))
     s.listen(5) # only queue 5 connections right now
-    
+
     while True:
         (client_socket, addr) = s.accept()
         client_socket.settimeout(TIMEOUT) # set timeout to prevent ddos
@@ -66,16 +69,16 @@ def listen_for_photos():
             filename = client_socket.recv(1024)
             filename = filename.decode("utf-8")
             print("received filename")
-            
+
             # ACK for filename received
             client_socket.send(b"SEND_FILE")
 
             # get photo data and write to file if verified
             job_dir = recv_photo(filename, client_sig_file, client_socket)
-            
+
             if os.path.exists(client_sig_file):
                 os.remove(client_sig_file)
-                
+
             client_socket.close()
 
             #### INVOKE JOB MANAGER HERE ####
@@ -84,8 +87,8 @@ def listen_for_photos():
             #jobmanager.update_job(0,job_dir+filename)
             jobmanager.update_job(0,jobmanager_job_dir)
             #################################
-            
-            
+
+
         except socket.timeout:
             print("connection timed out")
             client_socket.close()
@@ -97,7 +100,7 @@ def listen_for_photos():
         except:
             print("weird error")
             continue;
-        
+
     s.close()
     return
 
