fd8ce897526db2f852a66f5dd33faa27b61456d3
Joseph Volpe <jlv38@case.edu>
2018-05-06 22:26:01 -0400

    connect express and react properly

--- web/server.js
+++ web/server.js
@@ -4,12 +4,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use('/', index);
-app.use('/users', users);
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
 
 app.listen(4000);
 
