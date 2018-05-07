704f7530353161618318d2be6eefc5fe541c5a33
Joseph Volpe <jlv38@case.edu>
2018-05-07 01:49:21 -0400

    cleanup

--- web/client/src/components/AnalyzedInfo.js
+++ web/client/src/components/AnalyzedInfo.js
@@ -10,30 +10,15 @@ class AnalyzedInfo extends Component {
           jobID: props.jobID,
           height: props.height,
           width: props.width,
-          overlay: ''
+          overlay: 'http://localhost:4000/job/' + props.jobID + '/overlay/'
       };
   }
 
-  componentDidMount() {
-    this.getHeatmap()
-      .then(res => this.setState({ overlay: res }))
-      .catch(err => { console.log("eror"); console.log(err);});
-    console.log("mounted");
-  }
-
-  getHeatmap = async () => {
-    const overlay_get = /job/ + this.state.jobID + '/overlay/';
-    const response = await fetch(overlay_get);
-
-    return overlay_get;
-  };
-
   render() {
-      var overlay = 'http://localhost:4000' + this.state.overlay;
       return(
           <div className="image-display">
             <Image
-                  src={overlay}
+                  src={this.state.overlay}
                   height={ this.state.height }
                   width={ this.state.width }
             />
--- web/client/src/components/AnalyzedMap.js
+++ web/client/src/components/AnalyzedMap.js
@@ -7,33 +7,17 @@ class AnalyzedMap extends Component {
 
         this.state = {
             jobID: props.jobID,
-            heatmap: '',
+            heatmap: 'http://localhost:4000/job/' + props.jobID + '/heatmap/',
             height: props.height,
             width: props.width
         }
     }
 
-    componentDidMount() {
-      this.getHeatmap()
-        .then(res => this.setState({ heatmap: res }))
-        .catch(err => { console.log("eror"); console.log(err);});
-      console.log("mounted");
-    }
-
-    getHeatmap = async () => {
-      const heatmap_get = /job/ + this.state.jobID + '/heatmap/';
-      const response = await fetch(heatmap_get);
-
-      return heatmap_get;
-    };
-
   render() {
-      var heatmap = 'http://localhost:4000' + this.state.heatmap;
-      console.log(this.state.jobID);
       return (
           <div className="image-display">
               <div><Image
-                    src={heatmap}
+                    src={this.state.heatmap}
                     height={ this.state.height }
                     width={ this.state.width }
               /></div>
