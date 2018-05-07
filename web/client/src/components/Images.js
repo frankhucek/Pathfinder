17d3d0c20ed399f55ea102361c1ac4e03e9b2430
Joseph Volpe <jlv38@case.edu>
2018-05-07 01:58:34 -0400

    dynamic file access via express

--- web/client/src/components/Images.js
+++ web/client/src/components/Images.js
@@ -14,34 +14,35 @@ class Images extends Component {
     }
   }
 
-  importAllImages(filepath) {
-    var images = [];
-    
-    return images;
-  }
-
   componentDidMount() {
-    var filepath = /job/ + this.state.jobID + '/image/';
-    var all_images = this.importAllImages(filepath);
-    for (var image in all_images) {
-      var res = this.getImage(image);
-      var complete_link = 'http://localhost:4000' + res;
-      var joined = this.state.images.concat(
-        <div><Image
-          src={complete_link}
-          height={ this.state.height }
-          width={ this.state.width }
-        /></div>
-      );
-      this.setState({ images: joined });
-    }
+    this.getImageFileNames()
+      .then(res => {
+          console.log(res.images)
+          for (var image in res.images) {
+            var img_file = this.getImage(res.images[image]);
+            var joined = this.state.images.concat(
+              <div><Image
+                src={img_file}
+                height={ this.state.height }
+                width={ this.state.width }
+              /></div>
+            );
+            this.setState({ images: joined });
+          }
+      });
   }
 
-  getImage = async (image) => {
-    const image_get = /job/ + this.state.jobID + '/image/' + image;
-    const response = await fetch(image_get);
+  getImage(image) {
+    return 'http://localhost:4000/job/' + this.state.jobID + '/image/' + image;
+  }
 
-    return image_get;
+  getImageFileNames = async () => {
+    const filenames = /job/ + this.state.jobID + '/allimages/';
+    console.log(filenames);
+    const response = await fetch(filenames);
+    const body = response.json();
+
+    return body;
   };
 
   render() {
